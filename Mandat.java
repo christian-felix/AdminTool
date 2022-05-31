d095d331fd1db50c7fdaf3a95934bec6685a9bfe
Christian Felix <christian.felix@gmx.net>
2022-05-31 17:33:12 +0200

    Add files via upload
    Entities

---
+++ Mandat.java
@@ -0,0 +1,32 @@
+package entity;
+
+public class Mandat {
+
+    protected int id;
+    protected String name;
+    protected String path;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
